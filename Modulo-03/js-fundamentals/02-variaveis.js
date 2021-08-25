f99960bcd518a7f3e3af389a48c809d4db4f3f45
Leonardo <leonardo.costa.santos.si@gmail.com>
2021-08-24 22:47:26 -0300

    Módulo 03 - Javascript Basics 02 - Var, Let e Const

---
+++ Modulo-03/js-fundamentals/02-variaveis.js
@@ -0,0 +1,12 @@
+console.log(b)
+var b = 'Tulio' // hoisting
+
+let a = 10
+a = 30
+console.log(a)
+
+const firstName = 'Tulio'
+const lastName = 'Faria'
+const fullName = firstName + ' ' + lastName
+
+console.log(fullName)
