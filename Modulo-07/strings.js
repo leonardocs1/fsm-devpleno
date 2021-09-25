44d568404cd74d294032d0a9ba46c881e98aeb34
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-09-24 21:40:57 -0300

    Módulo 07 - Aprofudando no Js Pt 02 - Aula 01

---
+++ Modulo-07/strings.js
@@ -0,0 +1,12 @@
+const name = 'Tulio Faria'
+
+console.log(name.split(' ').join(' <=> '))
+
+const opa = `
+asdf
+Nome: ${name}
+1 + 1 = ${1 + 1}`
+
+const outra = name + ' ' + opa
+
+console.log(outra)
