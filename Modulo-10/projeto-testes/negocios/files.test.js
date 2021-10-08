03bc4cc09ccd2ea13028d243c9ff941b8b4a6c59
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-10-07 22:43:37 -0300

    Módulo 10 - Cobertura de Código 100%

--- Modulo-10/projeto-testes/negocios/files.test.js
+++ Modulo-10/projeto-testes/negocios/files.test.js
@@ -4,6 +4,10 @@ const fs = require('fs')
 const sinon = require('sinon')
 
 describe('reading directory', () => {
+  afterEach(done => {
+    fs.readdir.restore()
+    done()
+  })
   it('reads a directory', () => {
     sinon.stub(fs, 'readdir').callsFake((path, cb) => {
       cb(null, ['file1.txt'])
@@ -13,5 +17,16 @@ describe('reading directory', () => {
       expect(list.length).to.equal(1)
       expect(fs.readdir.getCall(0).args[0]).to.equal(path)
     })
+  })
+  it('fails reading a directory', () => {
+    sinon.stub(fs, 'readdir').callsFake((path, cb) => {
+      cb('error')
+    })
+    const path = './'
+    return files.readdir(path)
+      .catch(err => {
+        expect(err).to.equal('error')
+        expect(fs.readdir.getCall(0).args[0]).to.equal(path)
+      })
   })
 })
