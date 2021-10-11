cb7bbae2d412aabfc812b475801e0d3517acf65f
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-10-11 14:14:38 -0300

    Módulo 11 - ExpressJS: Rotas e Middlewares

---
+++ Modulo-11/proj-express/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const indexRouter = require('./routes/index')
+const clientRouter = require('./routes/clients')
+
+app.use((req, res, next) => {
+  console.log('passou por aqui')
+  next()
+})
+app.use('/', indexRouter)
+app.use('/clients', clientRouter)
+
+app.listen(port, () => console.log('Listening on port: ' + port))
