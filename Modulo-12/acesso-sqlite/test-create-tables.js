a1d43d153c2f839874b66d617a952392dce2cd8d
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-11-08 12:08:28 -0300

    Módulo 12 - SQLite 03 - CRUD Categoria

--- Modulo-12/acesso-sqlite/test-create-tables.js
+++ Modulo-12/acesso-sqlite/test-create-tables.js
@@ -22,27 +22,27 @@ const run = (db, query) => new Promise((resolve, reject) => {
 
 const createTables = async () => {
   const db = await initDB('banco.sqlite3')
-  //   await run(db, `
-  //     CREATE TABLE categories (
-  //       id INTEGER PRIMARY KEY NOT NULL,
-  //       category TEXT
-  //     );
-  //   `)
-  //   await run(db, `
-  // CREATE TABLE products (
-  //   id INTEGER PRIMARY KEY NOT NULL,
-  //   product TEXT,
-  //   price REAL
-  // );
-  // `)
-  //   await run(db, `
-  // CREATE TABLE images (
-  //   id INTEGER PRIMARY KEY NOT NULL,
-  //   description TEXT,
-  //   url TEXT,
-  //   product_id INTEGER REFERENCES products(id)
-  // );
-  // `)
+  await run(db, `
+      CREATE TABLE categories (
+        id INTEGER PRIMARY KEY NOT NULL,
+        category TEXT
+      );
+    `)
+  await run(db, `
+  CREATE TABLE products (
+    id INTEGER PRIMARY KEY NOT NULL,
+    product TEXT,
+    price REAL
+  );
+  `)
+  await run(db, `
+  CREATE TABLE images (
+    id INTEGER PRIMARY KEY NOT NULL,
+    description TEXT,
+    url TEXT,
+    product_id INTEGER REFERENCES products(id)
+  );
+  `)
   await run(db, `
   CREATE TABLE categories_products (
     product_id INTEGER REFERENCES products(id),
---
+++ Modulo-12/acesso-sqlite/test-update-category.js
@@ -0,0 +1,30 @@
+const sqlite3 = require('sqlite3').verbose()
+
+const initDB = databaseFile => new Promise((resolve, reject) => {
+  const db = new sqlite3.Database(databaseFile, (err) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(db)
+    }
+  })
+})
+
+const run = (db, query, values) => new Promise((resolve, reject) => {
+  db.run(query, values, err => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve()
+    }
+  })
+})
+
+const updateCategories = async () => {
+  const db = await initDB('banco.sqlite3')
+  await run(db, `update categories set category=? where id=?`, ['cat atualizada', 8])
+  console.log('Category updated')
+}
+updateCategories().catch(err => {
+  console.log(err)
+})
