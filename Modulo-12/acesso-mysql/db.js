77baa928d7810ce8bf026cfd1fc7bf2a5f42eea3
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-11-17 10:35:09 -0300

    Módulo 12 - MySQL 09 - Connection pool

--- Modulo-12/acesso-mysql/db.js
+++ Modulo-12/acesso-mysql/db.js
@@ -1,7 +1,18 @@
 const mysql = require('mysql2/promise')
 
-module.exports = mysql.createConnection({
+// without connection pool
+// module.exports = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'cat-products'
+// })
+
+// with connection pool
+module.exports = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'cat-products'
+  database: 'cat-products',
+  waitForConnection: true,
+  connectionLimit: 20,
+  queueLimit: 0
 })
