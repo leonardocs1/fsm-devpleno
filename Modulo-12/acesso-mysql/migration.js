2e452e16ce30b3abc0bc9dc0d71361d97d5c4a94
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-11-21 10:51:42 -0300

    Módulo 12 - Migrations 03 - Evoluindo (up)

--- Modulo-12/acesso-mysql/migration.js
+++ Modulo-12/acesso-mysql/migration.js
@@ -1,13 +1,58 @@
 const db = require('./db')
 const fs = require('fs')
 
+
+const initMigration = async (connection) => {
+  const [results] = await connection.query(`show tables like 'migration_version'`)
+  // nao iniciado
+  if (results.length === 0) {
+    await connection.query('START TRANSACTION;')
+    await connection.query(`
+      CREATE TABLE migration_version (
+        id INT NOT NULL AUTO_INCREMENT,
+        version INT NOT NULL,
+        PRIMARY KEY (id)
+      );
+    `)
+    await connection.query('INSERT INTO migration_version (id, version) VALUES (1, 0)')
+    await connection.query('COMMIT;')
+  }
+}
+
+const getCurrentVersion = async (connection) => {
+  const [results] = await connection.query('select * from migration_version where id = 1')
+  return results[0].version
+}
+
 const migration = async () => {
   const connection = await db
+  await initMigration(connection)
+
+  const currentVersion = await getCurrentVersion(connection)
+  const targetVersion = 1000
 
   const migrations = fs.readdirSync('./migrations')
-  for await (const migration of migrations) {
-    const m = require('./migrations/' + migration)
-    await m.down(connection)
+  const migrationSorted = migrations
+    .map(version => {
+      return version.split('.')[0]
+    })
+    .map(version => parseInt(version))
+    .sort((a, b) => {
+      if (a > b) {
+        return 1
+      }
+      return -1
+    })
+  for await (const migration of migrationSorted) {
+    if (migration > currentVersion && targetVersion >= migration) {
+      const m = require('./migrations/' + migration + '.js')
+      await connection.query('START TRANSACTION;')
+      if (m.up) {
+        await m.up(connection)
+      }
+      await connection.query('update migration_version set version = ? where id = ?', [migration, 1])
+      await connection.query('COMMIT;')
+    }
   }
 
   /*
