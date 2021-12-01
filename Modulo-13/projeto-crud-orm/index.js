ee49014b8e01736357626b40c7371b5a515a267c
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-01 17:34:43 -0300

    Módulo 13 - Projeto Prático: ORM - Iniciando o Projeto

---
+++ Modulo-13/projeto-crud-orm/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+const pessoas = require('./routes/pessoas')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index'))
+app.use('/pessoas', pessoas)
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+app.listen(port, () => {
+  console.log('CRUD-ORM Listening...')
+})
