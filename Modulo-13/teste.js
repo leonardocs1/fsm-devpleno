4117d412e995240f4c444e00c22e38206465ca7e
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-01 12:04:47 -0300

    Módulo 13 - ORM - Associações - Parte 2

--- Modulo-13/teste.js
+++ Modulo-13/teste.js
@@ -4,16 +4,39 @@ const sequelize = new Sequelize('sequelize-teste', 'root', '', {
   host: '127.0.0.1'
 })
 
+const Pessoa = sequelize.define('Pessoa', {
+  nome: Sequelize.STRING,
+  nascimento: Sequelize.DATE
+})
+
+const Projeto = sequelize.define('Projeto', {
+  nome: Sequelize.STRING
+})
+
 const Usuario = sequelize.define('Usuario', {
   usuario: Sequelize.STRING,
   senha: Sequelize.STRING
 })
+
+Pessoa.hasOne(Usuario)
+Usuario.belongsTo(Pessoa)
+Pessoa.hasMany(Projeto)
+Projeto.belongsTo(Pessoa)
+
 const testDB = async() => {
-  await sequelize.sync()
-  console.log('synced')
-  await Usuario.create({ 
-    usuario: 'Tulio Faria',
+  await sequelize.sync({force: true})
+  const pessoa = await Pessoa.create({
+    nome: 'Tulio Faria',
+    nascimento: '1985-08-04'
+  })
+  const user = await Usuario.create({
+    usuario: 'tuliofaria',
     senha: '123456'
   })
+  user.setPessoa(pessoa)
+  // await Usuario.create({ 
+  //   usuario: 'Tulio Faria',
+  //   senha: '123456'
+  // })
 }
 testDB()
