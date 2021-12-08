86bf2a53378bdb7a013f7874e204d423b9923e1a
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-08 14:46:09 -0300

    Módulo 13 - Projeto Prático: ORM - Editando e excluindo registros

--- Modulo-13/projeto-crud-orm/controllers/pessoas.js
+++ Modulo-13/projeto-crud-orm/controllers/pessoas.js
@@ -2,7 +2,6 @@ const index = async({Pessoa},  req, res) => {
   const pessoas = await Pessoa.findAll()
   res.render('pessoas/index', {pessoas})
 }
-
 const createForm = (req, res) => {
   res.render('pessoas/create')
 }
@@ -10,9 +9,31 @@ const createProcess = async({Pessoa}, req, res) => {
   await Pessoa.create(req.body)
   res.redirect('/pessoas')
 }
-
+const deleteOne = async({Pessoa}, req, res) => {
+  await Pessoa.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  res.redirect('/pessoas')
+}
+const editForm = async({Pessoa}, req, res) => {
+  const pessoa = await Pessoa.findByPk(req.params.id)
+  res.render('pessoas/edit', {pessoa})
+}
+const editProcess = async({Pessoa}, req, res) => {
+  await Pessoa.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  res.redirect('/pessoas')
+}
 module.exports = {
   index,
   createForm,
-  createProcess
+  createProcess,
+  deleteOne,
+  editForm,
+  editProcess
 }
--- Modulo-13/projeto-crud-orm/routes/pessoas.js
+++ Modulo-13/projeto-crud-orm/routes/pessoas.js
@@ -1,4 +1,4 @@
-const   express = require('express')
+const express = require('express')
 const pessoasController = require('../controllers/pessoas')
 const router = express.Router()
 
@@ -7,5 +7,8 @@ const model = require('../models/index')
 router.get('/', pessoasController.index.bind(null, model.models))
 router.post('/create', pessoasController.createProcess.bind(null, model.models))
 router.get('/create', pessoasController.createForm)
+router.get('/delete/:id', pessoasController.deleteOne.bind(null, model.models))
+router.post('/edit/:id', pessoasController.editProcess.bind(null, model.models))
+router.get('/edit/:id', pessoasController.editForm.bind(null, model.models))
 
 module.exports = router
