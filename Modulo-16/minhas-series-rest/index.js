de04bdf33965aa3b95fe15e6b407229c6a9c98eb
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-12 11:13:14 -0300

    Módulo 16 - Aula 01 - Autenticação com JWT - parte 1

---
+++ Modulo-16/minhas-series-rest/index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const mongo = process.env.MONGO || 'mongodb://localhost/minhas-series-rest'
+const User = require('./models/user')
+const jwt = require('jsonwebtoken')
+const jwtSecret = 'abc123abc123abc123'
+
+// bodyParser
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}))
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const series = require('./routes/series')
+
+app.use('/series', series)
+
+app.post('/auth', async (req, res) => {
+  const user = req.body
+  const userDb = await User.findOne({ username: user.username })
+  if (userDb) {
+    if (userDb.password === user.password) {
+      const payload = {
+        id: userDb._id,
+        username: userDb.username,
+        roles: userDb.roles
+      }
+      jwt.sign(payload, jwtSecret, (err, token) => {
+        res.send({
+          sucess: true,
+          token: token
+        })
+      })
+    } else {
+      res.send({ sucess: false, message: 'Wrong Credentials' })
+    }
+  } else {
+    res.send({ sucess: false, message: 'Wrong Credentials' })
+  }
+})
+
+const createInitialUsers = async () => {
+  const total = await User.count({})
+  if (total === 0) {
+    const user = new User({
+      username: 'leonardo',
+      password: '123456',
+      roles: ['restrito', 'admin']
+    })
+    await user.save()
+
+    const user2 = new User({
+      username: 'restrito',
+      password: '123456',
+      roles: ['restrito']
+    })
+    await user2.save()
+  }
+}
+
+mongoose
+  .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    createInitialUsers()
+    app.listen(port, () => console.log('listening on port: ' + port))
+  })
+  .catch(err => console.log(err))
