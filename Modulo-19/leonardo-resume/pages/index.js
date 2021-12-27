b4e0569fa5f6484eab53cde3fc1285efbd8540ef
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-27 17:19:36 -0300

    Módulo 19 - 05 - Adicionando o Tailwind

--- Modulo-19/leonardo-resume/pages/index.js
+++ Modulo-19/leonardo-resume/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Index = (props) => {
+const Index = ({ repos, user }) => {
   return (
-    <div>
-      <h1>Bem-vindo!</h1>
-      <div>{props.currentDate}</div>
-      {props.repos.map(repo => {
+    <div className='container mx-auto'>
+      <h1 className='text-5xl'>Olá, eu sou o Leonardo Costa</h1>
+      <h2 className='font-bold text-3xl'>Meus repositórios no GitHub</h2>
+      <p>Github stats: public repos: {user.public_repos} / public_gists: {user.public_gists} / followers: {user.followers}</p>
+      {repos.map(repo => {
         return (
-          <div key={repo.id}>
-            <h3>{repo.full_name}</h3>
+          <div key={repo.id} className='rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md'>
+            <h3 className='font-bold'>{repo.full_name}</h3>
             <p>{repo.language} / Stars: {repo.stargazers_count}</p>
           </div>
         )
@@ -18,6 +19,9 @@ const Index = (props) => {
 }
 
 export async function getServerSideProps(context) {
+  const resUser = await fetch('https://api.github.com/users/leonardocs1')
+  const user = await resUser.json()
+
   const resRepos = await fetch('https://api.github.com/users/leonardocs1/repos?sort=update')
   const originalRepos = await resRepos.json()
 
@@ -35,7 +39,8 @@ export async function getServerSideProps(context) {
   return {
     props: {
       currentDate: new Date().toString(),
-      repos
+      repos,
+      user,
     }
   }
 }
