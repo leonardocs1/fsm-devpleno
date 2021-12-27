b7ab7366bc01e87328bd150704b0a6ac3985469a
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-27 15:32:26 -0300

    Módulo 19 - 04 - Filtrando dados

--- Modulo-19/leonardo-resume/pages/index.js
+++ Modulo-19/leonardo-resume/pages/index.js
@@ -7,7 +7,7 @@ const Index = (props) => {
       <div>{props.currentDate}</div>
       {props.repos.map(repo => {
         return (
-          <div>
+          <div key={repo.id}>
             <h3>{repo.full_name}</h3>
             <p>{repo.language} / Stars: {repo.stargazers_count}</p>
           </div>
@@ -19,7 +19,19 @@ const Index = (props) => {
 
 export async function getServerSideProps(context) {
   const resRepos = await fetch('https://api.github.com/users/leonardocs1/repos?sort=update')
-  const repos = await resRepos.json()
+  const originalRepos = await resRepos.json()
+
+  const dontShowRepos = ['leonardocs1/fsm-devpleno', 'leonardocs1/leonardocs1']
+
+  const dontShowFilter = repo => dontShowRepos.indexOf(repo.full_name) === -1
+  const extractData = repo => ({
+    id: repo.id,
+    full_name: repo.full_name,
+    language: repo.language,
+    stargazers_count: repo.stargazers_count
+  })
+  const repos = originalRepos.filter(dontShowFilter).map(extractData)
+
   return {
     props: {
       currentDate: new Date().toString(),
