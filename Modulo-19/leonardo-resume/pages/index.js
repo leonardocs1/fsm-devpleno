9cc38d4905f1e37ce966ba69409dc17db3a18a1b
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-27 11:15:46 -0300

    Módulo 19 - 03 - Listando Repositórios

--- Modulo-19/leonardo-resume/pages/index.js
+++ Modulo-19/leonardo-resume/pages/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
-const Index = () => {
+const Index = (props) => {
   return (
     <div>
       <h1>Bem-vindo!</h1>
+      <div>{props.currentDate}</div>
+      {props.repos.map(repo => {
+        return (
+          <div>
+            <h3>{repo.full_name}</h3>
+            <p>{repo.language} / Stars: {repo.stargazers_count}</p>
+          </div>
+        )
+      })}
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const resRepos = await fetch('https://api.github.com/users/leonardocs1/repos?sort=update')
+  const repos = await resRepos.json()
+  return {
+    props: {
+      currentDate: new Date().toString(),
+      repos
+    }
+  }
 }
 
 export default Index
