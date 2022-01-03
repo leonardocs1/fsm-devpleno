aff8a6028788c0499f0ba71663e358c111f09eb0
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2022-01-03 17:29:47 -0300

    Módulo 22 - Put, Get, Post e Delete

--- Modulo-22/index.js
+++ Modulo-22/index.js
@@ -1,5 +1,39 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.delete('/products/:id', (req, res) => {
+  res.send({
+    sucess: true,
+    data: 'id' + req.params.id
+  })
+})
+
+app.patch('/products/:id', (req, res) => {
+  // alterar produtos - somente alguns campos
+  console.log(req.body)
+  res.send({
+    sucess: true,
+    data: req.body
+  })
+})
+
+app.put('/products/:id', (req, res) => {
+  // alterar products
+  console.log(req.body)
+  res.send({
+    sucess: true,
+    data: req.body
+  })
+})
+
+app.get('/products/:id', (req, res) => {
+  res.send({
+    name: 'Product 1'
+  })
+})
 
 app.get('/products', (req, res) => {
   res.send({
@@ -7,6 +41,13 @@ app.get('/products', (req, res) => {
   })
 })
 
+app.post('/products', (req, res) => {
+  console.log(req.body)
+  res.send({
+    sucess: true,
+    data: req.body
+  })
+})
 app.listen(3000, (err) => {
   if (err) {
     console.log('Not possible to listen on port 3000')
