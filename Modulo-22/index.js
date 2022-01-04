ffe32c550787d3c2051d462c8d02245299beb8da
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2022-01-03 21:12:58 -0300

    Módulo 22 - GraphQL Basics: Mutation Type

--- Modulo-22/index.js
+++ Modulo-22/index.js
@@ -11,12 +11,32 @@ app.use(routes)
 // graphql
 const typeDefs = gql`
   type Query {
-    getAllProducts: String
+    getAllProducts: [Product]
+  }
+  type Product {
+    id: String
+    name: String
+  }
+  type Mutation {
+    createProduct(input: ProductInput): Product
+  }
+  input ProductInput {
+    id: String!
+    name: String!
   }
 `
 const resolvers = {
   Query: {
-    getAllProducts: () => 'All products'
+    getAllProducts: () => [{ id: '1', name: 'All Products' }]
+  },
+  Mutation: {
+    createProduct: (context, { input }) => {
+      const { id, name } = input
+      console.log(id, name)
+      return {
+        id, name
+      }
+    }
   }
 }
 
