f7b01e40322aab43bd535522c202b3060236405d
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2022-01-05 09:28:58 -0300

    Módulo 22 - GraphQL: getAllProducts por categoria e criar produto

--- Modulo-22/graphql/resolvers/products.js
+++ Modulo-22/graphql/resolvers/products.js
@@ -1,9 +1,20 @@
-const getAllProducts = () => [{ id: '1', name: 'All Products' }]
-const createProduct = (context, { input }) => {
-  const { id, name } = input
-  console.log(id, name)
+const db = require('../../db')
+const Product = require('../../models/product')(db)
+
+const getAllProducts = async (context, { filter }) => {
+  let products = null
+  if (filter && filter.categoryId) {
+    products = await Product.findAllByCategory(filter.categoryId)
+  } else {
+    products = await Product.findAll()
+  }
+  return products
+}
+const createProduct = async (context, { input }) => {
+  const { product, price } = input
+  await Product.create([product, price])
   return {
-    id, name
+    product, price
   }
 }
 module.exports = { getAllProducts, createProduct }
