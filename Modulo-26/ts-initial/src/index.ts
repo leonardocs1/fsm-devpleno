78d81e64cf61eb627f29174125b771a0966ef099
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2022-01-12 19:42:26 -0300

    Módulo 26: 03 - Interfaces

--- Modulo-26/ts-initial/src/index.ts
+++ Modulo-26/ts-initial/src/index.ts
@@ -1,10 +1,28 @@
-function soma(a: number, b: number) {
-  return a + b
+interface Point {
+  x: number;
+  y: number;
 }
 
-const readFile = async () => {
-  return 10
+interface Point {
+  z: number;
 }
 
-let c = 'aa'
-c = soma(1, 2)
+// const pontoA: Point = { x: 1, y: 0 }
+
+interface Pessoa {
+  nome: string,
+  sexo: "m" | "f",
+  [key: string]: string | number;
+}
+
+const tulio: Pessoa = {
+  nome: 'Tulio',
+  sexo: 'm',
+  idade: '10',
+  teste: 10
+}
+
+function ola(pessoa: Pessoa) {
+  console.log('ola', pessoa.nome)
+}
+ola({ nome: 'teste', sexo: 'm', idade: 10 })
