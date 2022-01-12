a144fc10f7568957c352dc544b560343603b1312
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2022-01-12 15:22:55 -0300

    Módulo 25 - JWT - HoC e auth no GraphQL

--- Modulo-25/rest-graphql-apis/graphql/index.js
+++ Modulo-25/rest-graphql-apis/graphql/index.js
@@ -2,12 +2,30 @@ const { ApolloServer, gql } = require('apollo-server-express')
 const fs = require('fs')
 const path = require('path')
 const resolvers = require('./resolvers')
+const jwt = require('jsonwebtoken')
 
 const schema = fs.readFileSync(path.join(__dirname, './schema.graphql'))
 const typeDefs = gql`${schema}`
 
 const graphqlServer = new ApolloServer({
-  typeDefs, resolvers
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    if (req.headers && req.headers.authorization) {
+      const secret = 'leonardocs1'
+      const header = req.headers.authorization
+      const headerParts = header.split(' ')
+      try {
+        const payLoad = jwt.verify(headerParts[1], secret)
+        return { user: payLoad.user }
+      } catch (err) {
+
+      }
+    }
+    return {
+
+    }
+  }
 })
 
 module.exports = graphqlServer
--- Modulo-25/rest-graphql-apis/graphql/resolvers/index.js
+++ Modulo-25/rest-graphql-apis/graphql/resolvers/index.js
@@ -1,8 +1,19 @@
+const { AuthenticationError } = require('apollo-server-express')
 const { getAllCategories, createCategory, deleteCategory, updateCategory } = require('./category')
 const { getAllProducts, createProduct, deleteProduct, updateProduct, createImageOnProduct, deleteImageOnProduct } = require('./products')
+
+const needsAuth = resolver => {
+  return async (parent, args, context) => {
+    if (!context.user) {
+      throw new AuthenticationError('needs authentication')
+    }
+    return resolver(parent, args, context)
+  }
+}
+
 const resolvers = {
   Query: {
-    getAllProducts,
+    getAllProducts: needsAuth(getAllProducts),
     getAllCategories
   },
   Mutation: {
