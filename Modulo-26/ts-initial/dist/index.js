b1e83fdfb670a3cab579529306d106fb2838148f
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2022-01-12 20:00:34 -0300

    Módulo 26 - 04 - Classes

--- Modulo-26/ts-initial/dist/index.js
+++ Modulo-26/ts-initial/dist/index.js
@@ -1,10 +1,7 @@
-var tulio = {
-    nome: 'Tulio',
-    sexo: 'm',
-    idade: '10',
-    teste: 10
-};
-function ola(pessoa) {
-    console.log('ola', pessoa.nome);
-}
-ola({ nome: 'teste', sexo: 'm', idade: 10 });
+var Funcionario = /** @class */ (function () {
+    function Funcionario() {
+    }
+    return Funcionario;
+}());
+var func1;
+func1 = new Funcionario();
--- Modulo-26/ts-initial/src/index.ts
+++ Modulo-26/ts-initial/src/index.ts
@@ -1,28 +1,6 @@
-interface Point {
-  x: number;
-  y: number;
+class Funcionario {
+  nome: string;
 }
 
-interface Point {
-  z: number;
-}
-
-// const pontoA: Point = { x: 1, y: 0 }
-
-interface Pessoa {
-  nome: string,
-  sexo: "m" | "f",
-  [key: string]: string | number;
-}
-
-const tulio: Pessoa = {
-  nome: 'Tulio',
-  sexo: 'm',
-  idade: '10',
-  teste: 10
-}
-
-function ola(pessoa: Pessoa) {
-  console.log('ola', pessoa.nome)
-}
-ola({ nome: 'teste', sexo: 'm', idade: 10 })
+let func1: Funcionario
+func1 = new Funcionario()
